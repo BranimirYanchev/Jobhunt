0e6e43756c20fb2065f5d67a72df2557396567f6
BranimirYanchev <branimiryanchev@gmail.com>
2024-08-15 13:16:44 +0300

    restructure

--- doc/assets/js/ie.js
+++
@@ -1,8 +0,0 @@
-﻿(function () {
-    'use strict';
-    // cssVars({
-    //     onlyLegacy: false,
-    //     include: 'link[href="assets/css/main.css"],style',
-    //     onComplete: function (cssText, styleNode) {}
-    // });
-}());
