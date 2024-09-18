faa77cead343ed87cc0d0ae7172210a33f8d964c
BranimirYanchev <branimiryanchev@gmail.com>
2024-09-19 02:59:22 +0300

    working form

--- assets/elements/form/index.php
+++ assets/elements/form/index.php
@@ -1,20 +1,33 @@
-<div class="form-page">
-    <div class="form_container">
-      <i class="uil uil-times form_close"></i>
-      <!-- Login From -->
-      <div class="form login_form">
-        <form action="#">
-          <h2>Login</h2>
+<div class="form-page fade-in">
+  <div class="form_container">
+    <i class="uil uil-times form_close"></i>
 
-          <div class="input_box">
-            <input type="email" placeholder="Enter your email" required />
-            <i class="uil uil-envelope-alt email"></i>
-          </div>
-          <div class="input_box">
-            <input type="password" placeholder="Enter your password" required />
-            <i class="uil uil-lock password"></i>
-            <i class="uil uil-eye-slash pw_hide"></i>
-          </div>
+    <div class="form-headings">
+      <h2 class="login-part">Login Form</h2>
+      <h2 class="signup-part">Signup Form</h2>
+    </div>
+
+    <div class="form-slide-controls">
+        <input type="radio" name="slide" id="signup" checked="">
+        <input type="radio" name="slide" id="login">
+        <label for="login" class="slide login login-part">Login</label>
+        <label for="signup" class="slide signup c-b signup-part">Sign up</label>
+        <div class="slider-tab"></div>
+    </div>
+
+    <!-- Login From -->
+    <div class="form">
+      <form action="#" class="login-form">
+
+        <div class="input_box">
+          <input type="email" name="email" placeholder="Enter your email" required />
+          <i class="uil uil-envelope-alt email"></i>
+        </div>
+        <div class="input_box">
+          <input type="password" name="password" placeholder="Enter your password" required />
+          <i class="uil uil-lock password"></i>
+          <i class="uil uil-eye-slash pw_hide"></i>
+        </div>
 
         <div class="option_field">
           <span class="checkbox">
@@ -24,37 +37,32 @@
           <a href="#" class="forgot_pw">Forgot password?</a>
         </div>
 
-          <button class="button">Login Now</button>
+        <button class="button" id="login-btn">Login Now</button>
 
-          <div class="login_signup">Don't have an account? <a href="#" id="signup">Signup</a></div>
+        <div class="login_signup">Don't have an account? <a href="#" id="signup">Signup</a></div>
+      </form>
+
+      <form action="#" class="signup-form">
+        <div class="input_box">
+          <input type="text" name="username" placeholder="Enter your username" name="username" required />
+          <i class="uil uil-envelope-alt email"></i>
+        </div>
+        <div class="input_box">
+          <i class="uil uil-envelope-alt email"></i>
+          <input type="email" name="email"  placeholder="Enter your email" required />
+        </div>
+        <div class="input_box">
+          <input type="password" name="password" placeholder="Enter your password" required />
+          <i class="uil uil-lock password"></i>
+          <i class="uil uil-eye-slash pw_hide"></i>
+        </div>
+
+        <button class="button" id="signup-btn">Signup Now</button>
+
+        <div class="login_signup">Already have an account? <a id="login">Login</a></div>
         </form>
-      </div>
-
-      <!-- Signup From -->
-      <div class="form signup_form hide-form">
-        <form action="#">
-          <h2>Signup</h2>
-
-          <div class="input_box">
-            <input type="text" placeholder="Enter your username" name="username" required />
-            <i class="uil uil-envelope-alt email"></i>
-          </div>
-          <div class="input_box">
-            <i class="uil uil-envelope-alt email"></i>
-            <input type="email" placeholder="Enter your email" required />
-          </div>
-          <div class="input_box">
-            <input type="password" placeholder="Enter your password" required />
-            <i class="uil uil-lock password"></i>
-            <i class="uil uil-eye-slash pw_hide"></i>
-          </div>
-
-          <button class="button">Signup Now</button>
-
-          <div class="login_signup">Already have an account? <a id="login">Login</a></div>
-        </form>
-      </div>
     </div>
+  </div>
   <script src="assets/elements/form/script.js"></script>
   <script src="assets/elements/form/requests.js"></script>
 </div>
